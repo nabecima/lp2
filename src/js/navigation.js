a4a3c022e5349a4a63bb784c73aa290ea2de5a51
nabecima <nabecima8511gk@gmail.com>
2022-01-13 18:46:00 +0900

    ハンバーガーメニューを開いた時、スクロールさせないよう修正

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector(".l-header__burger");
 const nav = document.querySelector(".l-nav__links");
+const body = document.querySelector("#body");
 const active = "is-active";
 
 burger.addEventListener("click", function () {
   this.classList.toggle(active);
   nav.classList.toggle(active);
+  body.classList.toggle(active);
 });
