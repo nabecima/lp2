48246a55ffb5cf733de33adb0503ff7de10525c1
nabecima <nabecima8511gk@gmail.com>
2022-01-14 19:20:27 +0900

    headerとnavを固定

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,3 +1,4 @@
+const header = document.querySelector(".l-header");
 const burger = document.querySelector(".l-header__burger");
 const nav = document.querySelector(".l-nav__links");
 const body = document.querySelector("#body");
@@ -5,6 +6,7 @@ const active = "is-active";
 
 burger.addEventListener("click", function () {
   this.classList.toggle(active);
+  header.classList.toggle("shadow");
   nav.classList.toggle(active);
   body.classList.toggle(active);
 });
