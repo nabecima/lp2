bb928ad5f3f2bddac2003fdbaae779346afef159
nabecima <nabecima8511gk@gmail.com>
2022-01-31 19:08:44 +0900

    smoothscrollを実装

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,14 +1,22 @@
 const header = document.getElementById("header");
 const nav = document.getElementById("nav");
 const headerBtn = document.querySelector(".l-header__contact-btn");
+const heroBtn = document.querySelector(".p-hero__btn");
 const campaignBtns = document.querySelectorAll(".p-campaign__button--web");
+// const buttons = [headerBtn, heroBtn, campaignBtns];
 
+// buttons.forEach((btn) => {
+//   console.log(btn instanceof Array);
+// });
 headerBtn.addEventListener("click", function () {
   scroll("contact");
 });
 
+heroBtn.addEventListener("click", function () {
+  scroll("contact");
+});
+
 campaignBtns.forEach(function (btn) {
-  // debugger;
   btn.addEventListener("click", function () {
     scroll("contact");
   });
