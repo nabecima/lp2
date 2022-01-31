6956659cefebc1f315f78c569f60ac9088be7f0f
nabecima <nabecima8511gk@gmail.com>
2022-01-31 19:26:58 +0900

    レスポンシブ時にメニューをクリックしたらドロワーが閉じるように変更

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,7 @@
 const header = document.querySelector(".l-header");
 const burger = document.querySelector(".l-header__burger");
 const nav = document.querySelector(".l-nav__links");
+const navLinks = document.querySelectorAll(".l-nav__link");
 const body = document.querySelector("#body");
 const active = "is-active";
 
@@ -9,4 +10,15 @@ burger.addEventListener("click", function () {
   header.classList.toggle("shadow");
   nav.classList.toggle(active);
   body.classList.toggle(active);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    if (link.parentElement.parentElement.classList.contains(active)) {
+      burger.classList.toggle(active);
+      header.classList.toggle("shadow");
+      nav.classList.toggle(active);
+      body.classList.toggle(active);
+    }
+  });
 });
